2a3e8dd318919445405d38778928498c4c05c014
Joaquin Mendez <joaquin.enmt@gmail.com>
2025-05-19 14:38:41 -0400

    Feat: Agregados metodos post, pu y delete

--- clase_5_apis/gin/handlers/pelicula.go
+++ clase_5_apis/gin/handlers/pelicula.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"clase_5/gin/database"
 	db "clase_5/gin/database"
+	"clase_5/gin/dto"
 	"clase_5/gin/modelos"
 	"context"
 	"log"
@@ -54,5 +55,62 @@ func Get_pelicula_id(c *gin.Context) {
 
 func Post_pelicula(c *gin.Context) {
 
+	db := bun.NewDB(db.Conexion(), pgdialect.New())
+	var body dto.PeliculaDTO
+	if err := c.ShouldBind(&body); err != nil {
+		c.JSON(400, gin.H{"mensaje": "error en el body"})
+		return
+	}
+
+	if len(body.Nombre) == 0 {
+		c.JSON(400, gin.H{"mensaje": "debe ingresar el nombre"})
+		return
+	}
+
+	_, err := db.NewInsert().Model(&body).Exec(context.TODO())
+
+	if err != nil {
+		c.JSON(400, gin.H{"mensaje": "hubo un error inesperado"})
+		return
+	}
 	c.JSON(201, gin.H{"mensaje": "creado con exito"})
+}
+
+func Put_pelicula(c *gin.Context) {
+
+	db := bun.NewDB(db.Conexion(), pgdialect.New())
+	var body dto.PeliculaDTO
+
+	id := c.Param("id")
+
+	if err := c.Bind(&body); err != nil {
+		c.JSON(400, gin.H{"mensaje": "error en el body"})
+		return
+	}
+
+	model := modelos.PeliculaModel{Nombre: body.Nombre, Descripcion: body.Descripcion, TematicaId: body.TematicaId}
+	_, err := db.NewUpdate().Model(&model).Where("id=?", id).Exec(context.TODO())
+	if err != nil {
+		c.JSON(400, gin.H{"mensaje": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{
+		"mensaje": "actualizado con exito",
+	})
+}
+
+func Delete_pelicula(c *gin.Context) {
+
+	db := bun.NewDB(db.Conexion(), pgdialect.New())
+	var pelicula modelos.PeliculaModel
+	id := c.Param("id")
+
+	_, err := db.NewDelete().Model(&pelicula).Where("id=?", id).Exec(context.TODO())
+
+	if err != nil {
+		c.JSON(400, gin.H{"mensaje": "hubo un error inesperado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"mensaje": "borraro con exito"})
 }
